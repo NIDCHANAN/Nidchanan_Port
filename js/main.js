8195a162ec100d8302f8a0bb80b3708ff16d748d
NIDCHANAN <71490539+NIDCHANAN@users.noreply.github.com>
2024-07-08 19:35:23 +0700

    Update preloader smoot

--- js/main.js
+++ js/main.js
@@ -402,52 +402,6 @@ $(function() {
     },
   });
 
-  $('[data-fancybox="gallery"]').fancybox({
-    animationEffect: "zoom-in-out",
-    animationDuration: 600,
-    transitionDuration: 1200,
-    buttons: [
-      "zoom",
-      "share",
-      "slideShow",
-      "thumbs",
-      "close"
-    ],
-  });
-
-  $('[data-fancybox="diplome"]').fancybox({
-    animationEffect: "zoom-in-out",
-    animationDuration: 600,
-    transitionDuration: 1200,
-    buttons: [
-      "zoom",
-      "slideShow",
-      "thumbs",
-      "close",
-    ],
-  });
-
-  $('[data-fancybox="avatar"]').fancybox({
-    animationEffect: "zoom-in-out",
-    animationDuration: 600,
-    transitionDuration: 1200,
-    buttons: [
-      "zoom",
-      "close"
-    ],
-  });
-
-  $('[data-fancybox="recommendation"]').fancybox({
-    animationEffect: "zoom-in-out",
-    animationDuration: 600,
-    transitionDuration: 1200,
-    buttons: [
-      "zoom",
-      "slideShow",
-      "thumbs",
-      "close",
-    ],
-  });
 
   $.fancybox.defaults.hash = false;
 
